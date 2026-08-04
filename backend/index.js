c2033ee292d9476ad18cba8ba3861931340c9c96
marlonrod12@gmail.com <247076703+Noctis-arch@users.noreply.github.com>
2026-08-04 10:24:49 -0400

    Configure Express server

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 3000;
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("GameVault Backend is Running!");
