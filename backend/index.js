e3cb0a1136719a43499e7138199b58d62177d9ef
marlonrod12@gmail.com <247076703+Noctis-arch@users.noreply.github.com>
2026-08-04 10:49:58 -0400

    Connect backend to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 
+connectDB();
+
 app.use(cors());
 app.use(express.json());
-
-const PORT = process.env.PORT;
-
-app.get("/", (req, res) => {
-  res.send("GameVault Backend is Running!");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
