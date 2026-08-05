d6208c37ab73a9b3c86b63c224f49fbc784ae0d2
marlonrod12@gmail.com <247076703+Noctis-arch@users.noreply.github.com>
2026-08-05 15:59:43 -0400

    Pass game data through React Router state

--- frontend/src/components/GameCard.jsx
+++ frontend/src/components/GameCard.jsx
@@ -1,32 +1,33 @@
 import { Link } from "react-router-dom";
 
 function GameCard({ game }) {
-return (
-  <Link
-    to={`/game/${game.gameID}`}
-    className="game-link"
-  >
-    <div className="game-card">
-      <img src={game.thumb} alt={game.title} />
+  return (
+    <Link
+      to={`/game/${game.gameID}`}
+      state={{ game }}
+      className="game-link"
+    >
+      <div className="game-card">
+        <img src={game.thumb} alt={game.title} />
 
-      <div className="game-info">
-        <h3>{game.title}</h3>
+        <div className="game-info">
+          <h3>{game.title}</h3>
 
-        <p className="sale-price">
-          ${game.salePrice}
-        </p>
+          <p className="sale-price">
+            ${game.salePrice}
+          </p>
 
-        <p className="normal-price">
-          Regular: ${game.normalPrice}
-        </p>
+          <p className="normal-price">
+            Regular: ${game.normalPrice}
+          </p>
 
-        <p className="discount">
-          Save {Math.round(game.savings)}%
-        </p>
+          <p className="discount">
+            Save {Math.round(game.savings)}%
+          </p>
+        </div>
       </div>
-    </div>
-  </Link>
-);
+    </Link>
+  );
 }
 
 export default GameCard;
